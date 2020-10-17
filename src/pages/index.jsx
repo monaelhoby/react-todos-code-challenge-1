d081ea9fbedf49a67b9d4dc4c28c6739270709b4
mona mohamed <monaelhoby@yahoo.com>
2020-10-17 17:20:32 +0200

    get data for localStorage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import Header from '../components/Header';
 import ContentContainer from '../components/ContentContainer';
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
+import {store} from '../store/index'
+import {saveState} from '../localStrage/index';
+
+
+
+  // update state in store
+  store.subscribe(() => {
+    saveState({
+      list: store.getState().list
+    });
+  });
 
 function IndexPage() {
   return (
     <main>
+    <Provider store={store}>
       <Header />
       <ContentContainer>
         <TodoForm />
         <TodoList />
       </ContentContainer>
+    </Provider>
     </main>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,21 @@
 import { createStore } from 'redux';
 import Reducer from './reducer'
 
-
-export const store = createStore(Reducer)
+// put loadState in persistedState variable
+const loadState = () => {
+    try {
+        const data = localStorage.getItem('state');
+        if (data === null) {
+          return undefined;
+        }
+        return JSON.parse(data);
+      }  catch(err) {
+        // ignore write errors
+        throw new Error ("something is error")
+      }
+    }; 
+  
+   // to overrides the initial state put persistedState
+  // run store with reducer
+  export const store = createStore(Reducer,loadState())
+  
